9627dee26972653703d6b64f4e4f8f9cccc30a3b
Bilal Bşş <bilal530@gmail.com>
2020-02-15 23:47:06 +0300

    Add project files.

---
+++ betodds/Program.cs
@@ -0,0 +1,27 @@
+﻿using betodds;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+using System.Web;
+
+class Program
+{
+   
+    public static void Main(string[] args)
+    {
+        try
+        {
+            
+            Console.WriteLine(JsonConvert.DeserializeObject(oddsapiioAPI.makeAPICall()));
+
+
+        }
+        catch (WebException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+
+}
+
---
+++ betodds/oddsapiioAPI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace betodds
+{
+    public static class oddsapiioAPI
+    {
+        private static string API_KEY = "YOUR-API-KEY";
+
+        public static string makeAPICall()
+        {
+    
+        var URL = new UriBuilder("https://app.oddsapi.io/api/v1/odds");
+
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            queryString["sport"] = "soccer";
+            queryString["country"] = "england";
+            queryString["league"] = "soccer-england-premier-league";
+
+
+            URL.Query = queryString.ToString();
+
+            var client = new WebClient();
+            client.Headers.Add("apikey", API_KEY);
+            return client.DownloadString(URL.ToString());
+
+        }
+    }
+}
